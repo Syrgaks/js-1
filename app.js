d3e6b962628b009a732a6053ec91f318843dd14a
Syrgaks <majrambekovsyrgak@gmail.com>
2024-11-03 16:19:00 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// console.log('Hello word');
+// console.warn('warn');
+// console.info('info')
+
+//  ctrl + A
+
+
+// alert('hello word')
+//  prompt('Сенин атын ким?')
+// переменные ES var ES6 let, const
+//  const name = 'Maks'
+// const age = 25
+// const hobby = 'footboll'
+
+// console.log(name);
+// console.log(age);
+// console.log(hobby);
+//  const name = prompt('сенин атын ким?')
+//  const age = prompt('канча жаштасын?')
+//  const hobby = prompt('хоббин эмне? ')
+// // alert(`Менин атым ${name}, жашым${age}, менин хоббим ${hobby}`)
+//  console.log(`менин атым ${name}, жашым ${age}, менин хоббим ${hobby}`);
+
+// alert('азыр эки сан жазып аны кошобуз');
+//  const num = prompt('сан жаз');
+//  const num2 = prompt('дагы сан жаз');
+//  console.log(num+parseInt(num2));
+
+alert('азыр эки сан жазып аны кошобуз');
+const num = parseInt(prompt('сан жаз')) 
+const num2 = parseInt(prompt(' дагы сан жаз')) 
+
+console.log(num+num2)
